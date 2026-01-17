bdf56477c357e9e11e1394e0c7ae08e2d94012b6
Adarsh <adarshdhawale267@gmail.com>
2026-01-18 02:09:53 +0530

    all changes

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -13,48 +13,40 @@ const OurTeachers = () => {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-lg-3 d-flex mb-3 ftco-animate">
+                    <div className="col-lg-3 d-flex mb-3 ftco-animate align-items-stretch">
                         <div className="staff w-100 text-center p-3 shadow-sm border rounded bg-light h-100">
-                            <div className="info">
                                 <h3><a href="#">{t('teacher_1_name')}</a></h3>
                                 <span className="position mb-3 d-block">{t('teacher_1_role')}</span>
                                 <div className="text">
                                     <p>{t('teacher_1_desc')}</p>
                                 </div>
-                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-3 d-flex mb-3 ftco-animate">
+                    <div className="col-lg-3 d-flex mb-3 ftco-animate align-items-stretch">
                         <div className="staff w-100 text-center p-3 shadow-sm border rounded bg-light h-100">
-                            <div className="info">
                                 <h3><a href="#">{t('teacher_2_name')}</a></h3>
                                 <span className="position mb-3 d-block">{t('teacher_2_role')}</span>
                                 <div className="text">
                                     <p>{t('teacher_2_desc')}</p>
                                 </div>
-                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-3 d-flex mb-3 ftco-animate">
+                    <div className="col-lg-3 d-flex mb-3 ftco-animate align-items-stretch">
                         <div className="staff w-100 text-center p-3 shadow-sm border rounded bg-light h-100">
-                            <div className="info">
                                 <h3><a href="#">{t('teacher_3_name')}</a></h3>
                                 <span className="position mb-3 d-block">{t('teacher_3_role')}</span>
                                 <div className="text">
                                     <p>{t('teacher_3_desc')}</p>
                                 </div>
-                            </div>
                         </div>
                     </div>
-                    <div className="col-lg-3 d-flex mb-3 ftco-animate">
+                    <div className="col-lg-3 d-flex mb-3 ftco-animate align-items-stretch">
                         <div className="staff w-100 text-center p-3 shadow-sm border rounded bg-light h-100">
-                            <div className="info">
                                 <h3><a href="#">{t('teacher_4_name')}</a></h3>
                                 <span className="position mb-3 d-block">{t('teacher_4_role')}</span>
                                 <div className="text">
                                     <p>{t('teacher_4_desc')}</p>
                                 </div>
-                            </div>
                         </div>
                     </div>
                 </div>
